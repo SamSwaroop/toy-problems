ded776b9f11c453e7e33a5ca1b927372fda140f2
SamSwaroop <samswaroop97@msitprogram.net>
2021-04-06 20:50:39 +0530

    add constructor for LRU

---
+++ lru-cache.py
@@ -0,0 +1,21 @@
+class LRU:
+    def __init__(self,cache):
+        print("This is the init method")
+        self.cache=[]
+
+    def get(self):
+        print("This is the get method")
+        pass
+
+    def set(self):
+        print("This is the set method")
+        pass
+
+    def get_cache(self):
+        print("This is the get_cache method")
+        pass
+        
+
+
+def main():
+    s=LRU
